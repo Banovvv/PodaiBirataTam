96b3557045dc38235ff695a5e19977f9473b1c21
Banovvv <ivan_banov@abv.bg>
2022-08-28 09:48:54 +0300

    BeerType

---
+++ Data/PodaiBirataTam.Data.Models/BeerType.cs
@@ -0,0 +1,13 @@
+﻿namespace PodaiBirataTam.Data.Models
+{
+    public class BeerType
+    {
+        public BeerType()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
