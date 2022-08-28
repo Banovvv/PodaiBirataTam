7445ff3d0f0d8e6df8af4e1a7061f5fa6d311d95
Banovvv <ivan_banov@abv.bg>
2022-08-28 08:57:17 +0300

    Customer and role entities

---
+++ Data/PodaiBirataTam.Data.Models/Role.cs
@@ -0,0 +1,15 @@
+﻿namespace PodaiBirataTam.Data.Models
+{
+    public class Role
+    {
+        public Role()
+        {
+            Customers = new HashSet<Customer>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Customer> Customers { get; set; }
+    }
+}
