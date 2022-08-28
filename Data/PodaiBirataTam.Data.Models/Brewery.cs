1c307bae3e6c428830ccf14ef871de9bdee60256
Banovvv <ivan_banov@abv.bg>
2022-08-28 10:08:37 +0300

    Brewery

---
+++ Data/PodaiBirataTam.Data.Models/Brewery.cs
@@ -0,0 +1,18 @@
+﻿namespace PodaiBirataTam.Data.Models
+{
+    public class Brewery
+    {
+        public Brewery()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Website { get; set; }
+        public DateTime Since { get; set; }
+
+        public int AddressId { get; set; }
+        public virtual Address Address { get; set; }
+    }
+}
