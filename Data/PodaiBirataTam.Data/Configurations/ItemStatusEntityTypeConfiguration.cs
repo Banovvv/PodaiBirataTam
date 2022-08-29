1b40ed6c406449f4cbc7fac0f993d4490126d2cd
Banovvv <ivan_banov@abv.bg>
2022-08-29 08:14:43 +0300

    EntityTypeConfigurations

---
+++ Data/PodaiBirataTam.Data/Configurations/ItemStatusEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PodaiBirataTam.Data.Models;
+
+namespace PodaiBirataTam.Data.Configurations
+{
+    internal class ItemStatusEntityTypeConfiguration : IEntityTypeConfiguration<ItemStatus>
+    {
+        public void Configure(EntityTypeBuilder<ItemStatus> itemStatus)
+        {
+            itemStatus.Property(x => x.Status)
+                .IsRequired(true)
+                .IsUnicode(true);
+        }
+    }
+}
---
+++ Data/PodaiBirataTam.Data/Configurations/OrderStatusEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PodaiBirataTam.Data.Models;
+
+namespace PodaiBirataTam.Data.Configurations
+{
+    internal class OrderStatusEntityTypeConfiguration : IEntityTypeConfiguration<OrderStatus>
+    {
+        public void Configure(EntityTypeBuilder<OrderStatus> orderStatus)
+        {
+            orderStatus.Property(x => x.Status)
+                .IsRequired(true)
+                .IsUnicode(true);
+        }
+    }
+}
