e311160c83a3ff515a65d6a49ed5eac7ca58c99a
Banovvv <ivan_banov@abv.bg>
2022-08-29 14:35:14 +0300

    Changing structure for readibility

--- Data/PodaiBirataTam.Data/BeerDataContext.cs
+++ Data/PodaiBirataTam.Data/BeerDataContext.cs
@@ -24,20 +24,34 @@ namespace PodaiBirataTam.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Address-related configurations
             new AddressEntityTypeConfiguration().Configure(modelBuilder.Entity<Address>());
             new CountryEntityTypeConfiguration().Configure(modelBuilder.Entity<Country>());
             new TownEntityTypeConfiguration().Configure(modelBuilder.Entity<Town>());
+
+            // Beer-related configurations
+
+
+            // Customer-related configurations
+            new CustomerEntityTypeConfiguration().Configure(modelBuilder.Entity<Customer>());
+            new RoleEntityTypeConfiguration().Configure(modelBuilder.Entity<Role>());
+
+            // Invoice-related configurations
             new InvoiceEntityTypeConfiguration().Configure(modelBuilder.Entity<Invoice>());
             new InvoiceStatusEntityTypeConfiguration().Configure(modelBuilder.Entity<InvoiceStatus>());
+
+            // Order-related configurations
             new OrderEntityTypeConfiguration().Configure(modelBuilder.Entity<Order>());
             new OrderStatusEntityTypeConfiguration().Configure(modelBuilder.Entity<OrderStatus>());
             new ItemStatusEntityTypeConfiguration().Configure(modelBuilder.Entity<ItemStatus>());
+
+            // Payment-related configurations
             new PaymentEntityTypeConfiguration().Configure(modelBuilder.Entity<Payment>());
             new PaymentStatusEntityTypeConfiguration().Configure(modelBuilder.Entity<PaymentStatus>());
+
+            // Shipment-related configurations
             new ShipmentEntityTypeConfiguration().Configure(modelBuilder.Entity<Shipment>());
             new ShipmentStatusEntityTypeConfiguration().Configure(modelBuilder.Entity<ShipmentStatus>());
-            new CustomerEntityTypeConfiguration().Configure(modelBuilder.Entity<Customer>());
-            new RoleEntityTypeConfiguration().Configure(modelBuilder.Entity<Role>());
         }
     }
 }
