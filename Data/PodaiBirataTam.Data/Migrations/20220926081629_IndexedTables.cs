216d9d7520e4df7d3d63f27286da517c0e96e180
Ivan Gechev <ivan_banov@abv.bg>
2022-09-26 11:17:35 +0300

    Migrations

---
+++ Data/PodaiBirataTam.Data/Migrations/20220926081629_IndexedTables.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PodaiBirataTam.Data.Migrations
+{
+    public partial class IndexedTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TrackingNumber",
+                table: "Shipments",
+                type: "varchar(900)",
+                unicode: false,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)",
+                oldUnicode: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shipments_TrackingNumber",
+                table: "Shipments",
+                column: "TrackingNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Username",
+                table: "Customers",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Shipments_TrackingNumber",
+                table: "Shipments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_Username",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TrackingNumber",
+                table: "Shipments",
+                type: "varchar(max)",
+                unicode: false,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(900)",
+                oldUnicode: false);
+        }
+    }
+}
