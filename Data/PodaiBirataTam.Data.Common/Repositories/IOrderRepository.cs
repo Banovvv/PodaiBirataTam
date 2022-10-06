25378907f77604461656a7b09523313c4fb34310
Ivan Gechev <ivan_banov@abv.bg>
2022-10-06 17:20:10 +0300

    OrderRepository

--- Data/PodaiBirataTam.Data.Common/Repositories/IOrderRepository.cs
+++ Data/PodaiBirataTam.Data.Common/Repositories/IOrderRepository.cs
@@ -5,6 +5,6 @@ namespace PodaiBirataTam.Data.Common.Repositories
     public interface IOrderRepository
     {
         Task<IEnumerable<Order>> GetByStatusAsync(string status);
-        Task<IEnumerable<Order>> GetByUserAsync(string username);
+        Task<IEnumerable<Order>> GetByCustomerAsync(string username);
     }
 }
---
+++ Data/PodaiBirataTam.Data/Repositories/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PodaiBirataTam.Data.Common.Repositories;
+using PodaiBirataTam.Data.Models;
+
+namespace PodaiBirataTam.Data.Repositories
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(BeerDataContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetByStatusAsync(string status)
+        {
+            return await this.Context.Orders
+                .Include(x => x.Status)
+                .Where(x => x.Status.Status == status)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Order>> GetByCustomerAsync(string username)
+        {
+            return await this.Context.Orders
+                .Include(x => x.Customer)
+                .Where(x => x.Customer.Username == username)
+                .ToListAsync();
+        }
+    }
+}
