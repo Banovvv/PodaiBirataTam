17e2c27029cc6d358b89e6bd85956e21209ae47b
Banovvv <ivan_banov@abv.bg>
2022-10-07 18:49:49 +0300

    OrderService : IOrderService

---
+++ Services/PodaiBirataTam.Services.Data/Contracts/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using PodaiBirataTam.Data.Models;
+
+namespace PodaiBirataTam.Services.Data.Contracts
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<Order>> GetByStatusAsync(string status);
+        Task<IEnumerable<Order>> GetByCustomerAsync(string username);
+    }
+}
---
+++ Services/PodaiBirataTam.Services.Data/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using PodaiBirataTam.Data.Common.Repositories;
+using PodaiBirataTam.Data.Models;
+using PodaiBirataTam.Services.Data.Contracts;
+
+namespace PodaiBirataTam.Services.Data
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepository repository;
+
+        public OrderService(IOrderRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<IEnumerable<Order>> GetByCustomerAsync(string username)
+        {
+            return await this.repository.GetByCustomerAsync(username);
+        }
+
+        public async Task<IEnumerable<Order>> GetByStatusAsync(string status)
+        {
+            return await this.repository.GetByStatusAsync(status);
+        }
+    }
+}
