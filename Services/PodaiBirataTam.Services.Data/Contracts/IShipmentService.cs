13963b1381bfffebc7c8f5b4c880b9e4d98e6e17
Banovvv <ivan_banov@abv.bg>
2022-10-07 18:46:47 +0300

    ShipmentService and IShipmentService

---
+++ Services/PodaiBirataTam.Services.Data/Contracts/IShipmentService.cs
@@ -0,0 +1,10 @@
+﻿using PodaiBirataTam.Data.Models;
+
+namespace PodaiBirataTam.Services.Data.Contracts
+{
+    public interface IShipmentService
+    {
+        Task<Shipment?> GetByTrackingNumberAsync(string trackingNumber);
+        Task<IEnumerable<Shipment>> GetByStatusAsync(string status);
+    }
+}
---
+++ Services/PodaiBirataTam.Services.Data/ShipmentService.cs
@@ -0,0 +1,27 @@
+﻿using PodaiBirataTam.Data.Models;
+using PodaiBirataTam.Data.Repositories;
+using PodaiBirataTam.Services.Data.Contracts;
+
+namespace PodaiBirataTam.Services.Data
+{
+    public class ShipmentService : IShipmentService
+    {
+        private readonly ShipmentRepository repository;
+
+        public ShipmentService(ShipmentRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<Shipment?> GetByTrackingNumberAsync(string trackingNumber)
+        {
+            return await this.repository.GetByTrackingNumberAsync(trackingNumber);
+        }
+
+        public async Task<IEnumerable<Shipment>> GetByStatusAsync(string status)
+        {
+            return await this.repository.GetByStatusAsync(status);
+        }
+
+    }
+}
